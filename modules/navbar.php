bd56831c44c870e046ed678913c8fb9234d91091
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2022-01-07 12:03:07 -0500

    Update navbar.php
    Removed "external" and made wrldcoin.io link go to main site.

--- modules/navbar.php
+++ modules/navbar.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-        <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="30px" height="30px" /> wrldcoin.io</a>
+        <a class="navbar-brand" href="https://wrldcoin.io/index.php"><img src="assets/img/logo.png" width="30px" height="30px" /> wrldcoin.io</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,10 +8,12 @@
         <div class="collapse navbar-collapse" id="navbarColor02">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item dropdown">
+                    <!--
                     <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-toolbox text-white"></i> External</a>
                     <div class="dropdown-menu bg-primary" aria-labelledby="dropdown04">
                         <a class="dropdown-item text-white" href="https://nolex.org/main/WRLDTRX.html?ref=5f27039c6766fa6f943137be"> WRLD on NoleX</a>
                     </div>
+                    -->
                 </li>
                 <li class="nav-item"><a class="nav-link" href="index.php"><i class="fas fa-photo-video text-white"></i> Collection</a></li>
                 <li class="nav-item"><a class="nav-link" href="mint.php"><i class="fas fa-fire-alt text-white"></i> Mint NFT</a></li>
