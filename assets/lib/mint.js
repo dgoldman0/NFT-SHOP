4c74c3f0c929a3e382178b8e4b219d539d60c5f4
dgoldman0 <45105852+dgoldman0@users.noreply.github.com>
2022-01-11 07:39:16 -0500

    Update mint.js

--- assets/lib/mint.js
+++ assets/lib/mint.js
@@ -83,6 +83,7 @@ $(document).ready(function() {
     if (file != null) {
       console.log(file);
       readFile(file, function(res) {
+        console.log(res);
         $("image_select").attr("src", res);
       });
     } else {
